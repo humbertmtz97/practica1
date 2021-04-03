0da654527a61c4ba31ebcfdfd6e55969b78ce2c9
humbertmtz97 <humbertmtz97@gmail.com>
2021-04-03 09:01:15 -0600

    1645681

---
+++ humbertoscript.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+echo "Josae humberto";
+
+if [ $1 = 5 ];
+then
+echo"El valor es 5"
+fi
+
---
+++ script.sh
@@ -0,0 +1,12 @@
+`#!/bin/bash`
+echo "Jose humberto";
+
+if [ $1 = 5 ];
+then
+echo"El valor es 5"
+fi
+
+for i in 1 2 3 4 5
+do
+ echo "Hello $i"
+done
